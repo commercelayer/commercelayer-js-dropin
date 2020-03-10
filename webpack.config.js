cd863cf270ef9eb4736aef4fe9caa7a0673fcc81
Alessandro Casazza <alessandro@commercelayer.io>
2020-03-10 11:43:39 +0100

    Alessandro - chore(test): added service worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,25 @@
 const path = require('path')
+const WorkboxPlugin = require('workbox-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/main.ts',
   output: {
     filename: 'commercelayer.min.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist/'
+    path: path.resolve(__dirname, 'dist')
+    // publicPath: 'dist/'
   },
   resolve: {
     extensions: ['.ts', '.js', '.json'],
     modules: ['node_modules', path.resolve(__dirname, 'src')],
     symlinks: false
+  },
+  devServer: {
+    https: true,
+    // contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
   },
   module: {
     rules: [
@@ -31,5 +40,17 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+    // new WorkboxPlugin.GenerateSW({
+    //   // these options encourage the ServiceWorkers to get in there fast
+    //   // and not allow any straggling "old" SWs to hang around
+    //   clientsClaim: true,
+    //   skipWaiting: true
+    // })
+  ]
 }
